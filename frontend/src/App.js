547629dca0a1180d621a46ea4c65278ce216a3b0
Poorni007-ma <poornima@tecneural.com>
2026-03-11 15:20:32 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
+function App() {
+  const [message, setMessage] = useState("");
+  const [data, setData] = useState([]);
+  const API = process.env.REACT_APP_API_URL;
+  // const API="http://localhost:5000"
+  console.log(API,"apiisddss=================s===================")
+  useEffect(() => {
+    axios.get(`${API}/api/message`)
+      .then(res => setMessage(res.data.message))
+      .catch(err => console.error(err));
+
+    axios.get(`${API}/api/mysq`)
+      .then(res => setData(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>React Frontend</h1>
+      <h2>{message}</h2>
+      {data.map((user) => (
+        <div key={user.id}>
+          <h3>{user.name}</h3>
+          <h3>{user.email}</h3>
+        </div>
+      ))}
+
+    
+    </div>
+  );
+}
+
+export default App; 
